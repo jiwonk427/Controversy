9f2d8329fdcfc60e2c03db5500641da4f8f3dfbd
jiwon <jw.kim@itso.co.kr>
2023-05-23 15:30:49 +0900

    update 로고 및 콘텐츠 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <header class="w-full my-5 flex justify-center top-0 relative">
-        <img class="max-w-[210px] w-[80%]" src="/img/common/log.png" />
+    <header class="w-full py-5 flex justify-center top-0 relative">
+        <img class="max-w-[210px] w-[80%]" src="/img/common/logo.png" />
     </header>
 </template>
 
