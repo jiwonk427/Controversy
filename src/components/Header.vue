86c423b2f9e904d4635d9194a2fd0df790d4b847
오준호 <jh.oh@itso.co.kr>
2023-05-31 12:10:22 +0900

    GA 삽입

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <header class="w-full flex justify-center top-0 relative">
+        <a href="https://twitter.com/Alltest_/status/1661289489115885569?s=20" target="_blank"><img class="max-w-[40px] absolute top-7 left-5" src="/img/twitter.png"></a>
         <img class="max-w-[210px] w-[80%]" src="/img/common/logo.png" />
     </header>
 </template>
